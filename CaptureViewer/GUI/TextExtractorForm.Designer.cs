c683b051d48d008772363a94bd839c64ac76cb26
GunawanSiregar <gunawan.siregar@formulatrix.com>
2018-07-29 19:36:25 +0700

    Set form control to false

--- CaptureViewer/GUI/TextExtractorForm.Designer.cs
+++ CaptureViewer/GUI/TextExtractorForm.Designer.cs
@@ -90,6 +90,7 @@
       this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.ClientSize = new System.Drawing.Size(578, 644);
+      this.ControlBox = false;
       this.Controls.Add(this.TextBoxLogger);
       this.Controls.Add(this.menuStrip1);
       this.DoubleBuffered = true;
